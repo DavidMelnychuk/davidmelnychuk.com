0a2a3453de267bbf269e0b11cb60d0c75d73c4ac
David Melnychuk <davidm9779@gmail.com>
2020-03-04 17:09:54 -0800

    Update site contact details

---
+++ src/pages/about.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Layout from "../components/layout"
+
+const AboutPage = () => {
+    return (
+        <Layout>
+            <h1>About</h1>
+            <p>I'm currently a fourth-year student at Simon Fraser University.</p>
+        </Layout>
+    )
+}
+
+export default AboutPage
