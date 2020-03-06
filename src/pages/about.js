b7d7ce5a6973d1ae6393adc9f393e44a3127f8c6
David Melnychuk <davidm9779@gmail.com>
2020-03-05 19:41:16 -0800

    update about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,13 +1,22 @@
-import React from 'react'
+import React from "react"
 import Layout from "../components/layout"
 
 const AboutPage = () => {
-    return (
-        <Layout>
-            <h1>About</h1>
-            <p>I'm currently a fourth-year student at Simon Fraser University.</p>
-        </Layout>
-    )
+  return (
+    <Layout>
+      <h1>About Me</h1>
+      <p>
+        I'm David Melnychuk, a fourth-year computing science student at SFU and
+        avid learner.
+      </p>
+
+      <p>
+        This website is a place for me to document everything I learn through
+        life. I wanted to create a record of the things I've learned and done so
+        I can better reflect on my past and measure my progress.
+      </p>
+    </Layout>
+  )
 }
 
 export default AboutPage
