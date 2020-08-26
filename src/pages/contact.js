9c0d222d25a1b2b87c020725b9f0aa0969011799
David Melnychuk <davidm9779@gmail.com>
2020-08-26 10:07:08 -0700

    remove social media, update journal

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -11,34 +11,12 @@ const ContactPage = () => {
         Have a question, comment, or just want to say hello? Reach out and let's
         talk!
       </p>
-      {/* TODO: Make contacts metadata tags */}
       <div>
         <h3>Send an email</h3>
         <p>
           <a href="mailto:david@davidmelnychuk.com">david@davidmelnychuk.com</a>
         </p>
       </div>
-
-      <div>
-        <h3> Find me online</h3>
-      </div>
-      <ul>
-        <li>
-          <strong>
-            <a href="https://www.linkedin.com/in/david-melnychuk/">LinkedIn</a>
-          </strong>
-        </li>
-        <li>
-          <strong>
-            <a href="https://github.com/DavidMelnychuk">Github</a>
-          </strong>
-        </li>
-        <li>
-          <strong>
-            <a href="https://twitter.com/david_melnychuk">Twitter</a>
-          </strong>
-        </li>
-      </ul>
     </Layout>
   )
 }
